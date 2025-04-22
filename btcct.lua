b0a8f8f7f974c15d8ab737231341a32896f6b48f
Jannnn13 <jannik@haus16.de>
2025-04-22 21:27:56 +0200

    Updates license information
    Replaces the existing license text with the updated BTCCT version.
    
    Ensures compliance with the current licensing terms.

---
+++ btcct.lua
@@ -0,0 +1,62 @@
+--[[
+    BTCCT - A better Terminal for CC: Tweaked
+    github.com/Jannnn13/btcct
+
+    Author: Jannnn13 (github.com/Jannnn13)
+    License: MIT License
+]]
+
+local msid = multishell.getCurrent()
+multishell.setTitle(msid, "BTCCT")
+
+-- Define rc as an empty table by default
+local rc = {}
+
+-- Load rc file if it exists
+if fs.exists("/.btcct.rc.lua") then
+    rc = require("/.btcct.rc.lua")
+end
+
+-- Configuration with defaults
+local cfg = {
+    written = rc.written or "> ",
+    written_top = rc.written_top or nil,
+    show_path = rc.show_dir or true,
+    term_color = rc.term_color or colors.white,
+    term_secondary_color = rc.term_secondary_color or colors.blue,
+    multishell_title = rc.multishell_title or nil,
+    about_text = rc.about_text or os.version(),
+    start_dir = rc.start_dir or nil,
+    auto_completion = rc.auto_completion or true,
+    path_in_writtentop = rc.path_in_writtentop or false,
+}
+
+term.setTextColor(cfg.term_secondary_color)
+print(cfg.about_text)
+
+if cfg.start_dir then shell.setDir(cfg.start_dir) end
+
+while true do
+    term.setTextColor(cfg.term_secondary_color)
+    
+    if shell.dir() == "/" or not cfg.show_path then
+        write(cfg.written)
+    else
+        write(shell.dir() .. cfg.written)
+    end
+    
+    term.setTextColor(cfg.term_color)
+
+    -- Use read() with autocomplete
+    if cfg.auto_completion then
+        input = read(nil, nil, shell.complete)
+    else
+        input = read()
+    end
+
+    -- Checks if the user wants to exit
+    if input == "exit" then break end
+
+    -- Runs the program
+    shell.run(input)
+end
